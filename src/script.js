df21bed6399c31fc3ac655dc57913c09ba8bcc2b
swalzem <45475951+swalzem@users.noreply.github.com>
2020-05-11 01:38:33 +0100

    Removed static HTML and added forecast loop in JS

--- src/script.js
+++ src/script.js
@@ -35,10 +35,58 @@ function currentLocationTemperature() {
   navigator.geolocation.getCurrentPosition(currentLocationSearch);
 }
 
+function formatHours(timestamp) {
+  let currentTime = new Date(timestamp);
+  let hours = currentTime.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+  let minutes = currentTime.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  return `${hours}:${minutes}`;
+}
+
+function displayForecast(response) {
+  let forecastElement = document.querySelector("#forecast");
+  forecastElement.innerHTML = null;
+  let forecast = null;
+
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    forecastElement.innerHTML += `
+        <div class="col-2">
+          <div class="row justify-content-center forecast-day-of-the-week">
+            <h3>${formatHours(forecast.dt * 1000)}</h3>
+          </div>
+          
+          <div class="row justify-content-center forecast-weather-image">
+            <img
+              src="http://openweathermap.org/img/wn/${
+                forecast.weather[0].icon
+              }@2x.png"
+              alt=""
+            />
+          </div>
+          <div class="row justify-content-center forecast-high">
+            <strong>${Math.round(forecast.main.temp_max)}°</strong>
+          </div>
+          <div class="row justify-content-center forecast-low">
+            ${Math.round(forecast.main.temp_min)}°
+          </div>
+        </div>`;
+  }
+}
+
 function search(city) {
   let apiKey = "c99a8d499a1f61b742240fa4afade60a";
   let apiUrl = `http://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
   axios.get(`${apiUrl}&appid=${apiKey}`).then(showTemperature);
+
+  apiUrl = `http://api.openweathermap.org/data/2.5/forecast?q=${city}&units=metric`;
+  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayForecast);
 }
 
 function handleSubmit(event) {
@@ -88,17 +136,7 @@ function formatDate(timestamp) {
 }
 
 function formatTime(timestamp) {
-  let currentTime = new Date(timestamp);
-  let hours = currentTime.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-  let minutes = currentTime.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
-  }
-
-  return `${hours}:${minutes}`;
+  return `${formatHours(timestamp)}`;
 }
 
 function tempConvertCelsius(event) {
